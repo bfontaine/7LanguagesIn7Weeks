f26854391d81c1adcc8205eb70b3fff647ab4158
Baptiste Fontaine <batifon@yahoo.fr>
2012-05-17 00:29:44 +0200

    bash script added to update Io

---
+++ update_io.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd /tmp
+echo -n "Receiving archive... "
+wget https://github.com/stevedekorte/io/zipball/master -O io.zip && echo "ok" || echo "fail! "
+echo -n "Unzipping... "
+unzip io.zip && rm -f io.zip && echo "ok" || echo "fail! "
+cd stevedekorte-*
+echo -n "Building... "
+./build.sh && echo "ok" || echo "fail! "
+sudo ./build install
+cd ..
+rm -Rf stevedekorte-*
